26d9d27048f49e85fcc6b1c85b64c5941c68a26c
alegarciy <ale.garciy@gmail.com>
2022-03-30 15:22:16 -0600

    Clase nodo creada

---
+++ Tablero/nodo.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+
+from Tablero.tablero import Tablero
+
+@dataclass(frozen=True)
+class Nodo:
+  tablero: Tablero= Tablero(ancho=0, vehiculos=tuple())
+  profundidad: int= 0
+  valor: int= 0
+
+  def __lt__(self, otroNodo):
+    return self.valor < otroNodo.valor
+
+@dataclass(frozen=True)
+class childNode(Node):
+  parent: Nodo = Nodo()
