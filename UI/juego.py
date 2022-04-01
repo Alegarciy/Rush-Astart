58dce80612e7ac4fae8baac1897d474ecac3dbcf
Mariell <mariellsanchez@estudiantec.cr>
2022-04-01 11:09:55 -0600

    Start screen and file selector

--- UI/juego.py
+++ UI/juego.py
@@ -23,6 +23,14 @@ coloresDeAutos = {
     18: (214, 176, 145)
 }
 
+WHITE = (255,255,255)
+BLACK = (  0,  0,  0)
+
+RED   = (255, 87, 87) 
+GREEN = (  0,255,  0)
+
+YELLOW = (255, 222, 89)
+
 
 def grid(ventana, tamano, numeroDeFilas):
     distanciaEntreFilas = tamano // numeroDeFilas
@@ -52,16 +60,67 @@ def redraw(ventana):
     ventana.fill((255, 255, 255))
 
 
-def main():
-    global tamano, numeroDeFilas
+def button_create(text, rect, inactive_color, active_color, action):
+
+    font = pygame.font.Font(None, 40)
+
+    button_rect = pygame.Rect(rect)
+
+    text = font.render(text, True, BLACK)
+    text_rect = text.get_rect(center=button_rect.center)
+
+    return [text, text_rect, button_rect, inactive_color, active_color, action, False]
+
+
+def button_check(info, event):
+
+    text, text_rect, rect, inactive_color, active_color, action, hover = info
+
+    if event.type == pygame.MOUSEMOTION:
+        # hover = True/False   
+        info[-1] = rect.collidepoint(event.pos)
+
+    elif event.type == pygame.MOUSEBUTTONDOWN:
+        if hover and action:      
+            action()
+
+
+def button_draw(screen, info):
+
+    text, text_rect, rect, inactive_color, active_color, action, hover = info
+
+    if hover:
+        color = active_color
+    else:
+        color = inactive_color
+
+    pygame.draw.rect(screen, color, rect)
+    screen.blit(text, text_rect)
+
+
+def game():
+    
+    global tamano, numeroDeFilas, seguirJugando
     tamano = 500
     numeroDeFilas = 6
 
-    ventana = pygame.display.set_mode((tamano, tamano))
+    ventana = pygame.display.set_mode((500, 650))
     ventana.fill((255, 255, 255))
+    
     grid(ventana, tamano, numeroDeFilas)
-    pygame.display.update()
 
+    font = pygame.font.Font(None, 32)
+    clock = pygame.time.Clock()
+    input_box = pygame.Rect(55, 540, 200, 40)
+    color_inactive = (71,71,71)
+    color_active = (20,20,20)
+    color = color_inactive
+    active = False
+    text = ''
+    
+    button_return = button_create("RETURN", (150, 590, 200, 40), RED, YELLOW, on_click_button_return)
+
+    pygame.display.update()
     seguirJugando = True
 
     # Loop principal del juego
@@ -70,8 +129,107 @@ def main():
         for evento in pygame.event.get():
             if evento.type == pygame.QUIT:
                 exit()
+            if evento.type == pygame.MOUSEBUTTONDOWN:
+                # If the user clicked on the input_box rect.
+                if input_box.collidepoint(evento.pos):
+                    # Toggle the active variable.
+                    active = not active
+                else:
+                    active = False
+                # Change the current color of the input box.
+                color = color_active if active else color_inactive
+            if evento.type == pygame.KEYDOWN:
+                if active:
+                    if evento.key == pygame.K_RETURN:
+                        print(text)
+                        text = ''
+                    elif evento.key == pygame.K_BACKSPACE:
+                        text = text[:-1]
+                    else:
+                        text += evento.unicode
+        
+        #ventana.fill((30, 30, 30))
+        # Render the current text.
+        
+        txt_label = font.render("Select the file here:", 1, (71,71,71))
+        ventana.blit(txt_label, (55, 515))
+        
+        txt_box = font.render(text, True, color)
+        # Resize the box if the text is too long.
+        
+        width = max(400, txt_box.get_width()+10)
+        input_box.w = width
+        # Blit the text.
+        ventana.blit(txt_box, (input_box.x+5, input_box.y+5))
+        # Blit the input_box rect.
+        pygame.draw.rect(ventana, color, input_box, 2)
+        #grid(ventana, tamano, numeroDeFilas)
+        
+        button_check(button_return, evento)
+        button_draw(ventana, button_return)
 
-        redraw(ventana)
+        clock.tick(30)
+        
+        pygame.display.flip()
+        
+        #redraw(ventana)
+    # para tomar el nombre del file es text
+    # print(text)
+
+def on_click_button_1():
+    print('You clicked Button 1')
+    game()
 
 
+def on_click_button_3():
+    print('You exited the game')
+    global running, seguirJugando
+    running = False
+    seguirJugando = False
+
+def on_click_button_return():
+    print('You clicked Button Return')
+    main()
+    
+
+
+def main():
+    global running
+    running = True
+    ventana = pygame.display.set_mode((500, 650))
+    ventana.fill((255, 255, 255))
+    screen_rect = ventana.get_rect()
+
+    button_1 = button_create("START", (150, 275, 200, 100), RED, YELLOW, on_click_button_1)
+    button_3 = button_create("EXIT", (150, 425, 200, 100), RED, YELLOW, on_click_button_3)
+    
+    background = pygame.image.load("UI/background.png")
+
+    
+    while running:
+
+        # - events -
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+            button_check(button_1, event)
+            button_check(button_3, event)
+            #    pass
+
+
+        ventana.fill(BLACK)
+        ventana.blit(background, (0,0))
+        button_draw(ventana, button_1)
+        button_draw(ventana, button_3)
+
+        pygame.display.update()
+
+
+    
+
+
+pygame.init()
 main()
+pygame.quit()
